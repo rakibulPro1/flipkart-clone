80531b2cf37cc4b9dcdbf4b565fbde5182f74abe
Rakibul Islam <rakibul102760@gmail.com>
2022-03-07 00:30:19 +0600

    Anis-dev is starting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App">{/* Anis dev is starting this job */}</div>;
 }
-
 
 export default App;

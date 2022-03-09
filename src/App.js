cce6753fc1b0e8d4936399db2f2871d5ed85d4fd
Rakibul Islam <rakibul102760@gmail.com>
2022-03-09 15:12:07 +0600

    flipkart-clone-setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 
 function App() {
-  
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className=""></div>;
 }
-
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
